c1c533c6a780d806bb6e8e02ef91defb7b92f185
zeraye <zeraye@protonmail.com>
2023-10-30 15:58:51 +0100

    feat: remove clipper2

--- pkg/offset/offset.go
+++ pkg/offset/offset.go
@@ -5,9 +5,7 @@ import (
 )
 
 func CreateOffset(poly *geom.Polygon, offset float64, offsetAlgorithm string) []*geom.Polygon {
-	if offsetAlgorithm == "goclipper2" {
-		return createOffsetGoclipper2(poly, offset)
-	} else if offsetAlgorithm == "custom1" {
+	if offsetAlgorithm == "custom1" {
 		return createOffsetCustom1(poly, offset)
 	} else if offsetAlgorithm == "custom2" {
 		return createOffsetCustom2(poly, offset, "miter")
