02fd821250f7d6f307c1bb2bd16a8ef733ca72ff
zeraye <zeraye@protonmail.com>
2023-11-20 19:16:29 +0100

    refactor: add changes to allow fyne-cross

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ LD_LIBRARY_PATH=lib
 export LD_LIBRARY_PATH
 
 build:
-	go build -o bin/polygon-editor cmd/polygon-editor/main.go 
+	go build -o bin/polygon-editor
 
 run: build
 	./bin/polygon-editor
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	polygon_editor "github.com/zeraye/polygon-editor"
 	"github.com/zeraye/polygon-editor/pkg/config"
 )
 
@@ -17,7 +16,7 @@ func main() {
 
 	app := app.New()
 	window := app.NewWindow(config.Window.Name)
-	game := polygon_editor.NewGame(config)
+	game := NewGame(config)
 
 	window.SetContent(game.BuildUI())
 	window.Resize(fyne.NewSize(float32(config.Window.Width), float32(config.Window.Height)))
